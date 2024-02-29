4a48a1fc7fdcd46224b87d570e4624fcdf79ae58
Faisal-Aijjaz <fm9196298@gmail.com>
2024-02-29 17:53:50 +0500

    Chapter 1 Completed

--- Chapter 1/app.js
+++ Chapter 1/app.js
@@ -1,1 +1,15 @@
-alert('HELLO WORLD')
+//Q1-Write a script to greet your website visitor using js alert box:
+alert("HELLO") 
+
+//Q2-Write a script to display following message on your webpage page:
+alert("Error! Please enter a valid password.")
+
+//Q3-Write a script to display following message on your web page: 
+alert("Welcome to JS Land...\n Happy Coding!")
+
+//Q4-Write script to display following messages in sequence:
+alert("Welcome to JS Land...")
+alert("Happy Coding!")
+
+//Q5-Generate the following massage through browser's developer console:
+console.log("Hello...I can run JS through my web browser's console.")
