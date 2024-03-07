90b0e67b235641dd7b9e248236b3a35667b3d1f1
Faisal-Aijjaz <fm9196298@gmail.com>
2024-03-07 21:01:36 +0500

    Chapter 3 Completed

---
+++ Chapter 3/app.js
@@ -0,0 +1,18 @@
+//Q1-Declare a Variable Called age & assign to it your age.Show your age in an alert box.
+var age = 19
+alert("My age is " + age)
+
+//Q2-Declare a variable called birth year & assign it to your birth year. Show the following message in your browser.
+var birthYear = 2004
+document.write(`<li> My Birth Year is ${birthYear}</li>`)
+document.write(`<li> Datatype of my declared variable is number.</li>`)
+
+//Q3-A visitor visits an online clothing store www.xyzclothing.com.Write a Script to store variables the following information:
+//a)Visitor's name
+//b)Product title
+//Quantity i.e how many products a visitor wants to order.
+//Show the following message in Your browser:"John Doe ordered 5T-Shirts on XYZ Clothing Center".
+var visitorName = "John Doe"
+var product = "XYZ Clothing Center"
+var quantity = 5
+document.write(`<li>${visitorName} ordered ${quantity}T-Shirts on ${product}.</li>`)
